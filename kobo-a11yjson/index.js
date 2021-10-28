64c46f8ce66206e9b1c0bd921c9f47fb81b1994e
Laurens <8708858+Razpudding@users.noreply.github.com>
2021-10-28 12:43:37 +0200

    Now loads a csv file and pipes the results

--- kobo-a11yjson/index.js
+++ kobo-a11yjson/index.js
@@ -1,6 +1,35 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello typescript");
 const fs_1 = require("fs");
-let arr = new Array();
-(0, fs_1.createReadStream)('speciesData/soorten_uitzonderingen.csv');
+const csv_parser_1 = __importDefault(require("csv-parser"));
+const inputSrc = 'kobodata/Toegankelijkheidsscan_gebouwen_test.csv';
+let results = [];
+loadSurveyData(inputSrc);
+/**
+ * Loads a csv sourcefile and pipes results to processResults
+ * @param src path to the csv file to load
+ * @returns nothing for now
+ * @todo Remove side-effect and use promises instead
+ * @todo check if type can be csvfile 🤔
+ */
+function loadSurveyData(src) {
+    (0, fs_1.createReadStream)(src)
+        .pipe((0, csv_parser_1.default)())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+        processResults(results);
+    });
+}
+/**
+ * Loads processes results and starts conversion to a11yjson
+ * @param results survey results
+ * @returns nothing for now
+ */
+function processResults(results) {
+    console.log(results.length);
+    const lastItem = results.slice(-1);
+    console.log(lastItem);
+}
--- kobo-a11yjson/index.ts
+++ kobo-a11yjson/index.ts
@@ -1,6 +1,35 @@
-console.log("hello typescript")
 import { Accessibility, PlaceInfo } from '@sozialhelden/a11yjson'
 import { createReadStream, writeFile} from 'fs'
-let arr = new Array()
+import csv from 'csv-parser'
 
-createReadStream('speciesData/soorten_uitzonderingen.csv')
+const inputSrc:string = 'kobodata/Toegankelijkheidsscan_gebouwen_test.csv'
+let results:object[] = []
+
+loadSurveyData(inputSrc)
+
+/**
+ * Loads a csv sourcefile and pipes results to processResults
+ * @param src path to the csv file to load
+ * @returns nothing for now
+ * @todo Remove side-effect and use promises instead
+ * @todo check if type can be csvfile 🤔
+ */
+function loadSurveyData(src:string):void{
+	createReadStream(src)
+	  .pipe(csv())
+	  .on('data', (data) => results.push(data))
+	  .on('end', () => {
+	  	processResults(results)
+	  })
+}
+
+/**
+ * Loads processes results and starts conversion to a11yjson
+ * @param results survey results
+ * @returns nothing for now
+ */
+function processResults(results:object[]){
+	console.log(results.length)
+	const lastItem:object = results.slice(-1)
+	console.log(lastItem)
+}
